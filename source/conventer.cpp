9a18f8cd4afa50ec57b87aaf5f6a98926df751dd
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-23 20:49:30 -0300

    Create conventer.cpp

---
+++ source/conventer.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "converter.hpp"
+
+void convertZipToCIA(const char* zipPath) {
+
+    printf("Convertendo: %s\\n", zipPath);
+
+    // Aqui futuramente:
+    // Extrair ZIP
+    // Compilar projeto
+    // Gerar CIA
+
+    printf("CIA gerado!\\n");
+}
